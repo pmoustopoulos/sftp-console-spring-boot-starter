fa973ea326204438b700acf1c10895f71965e116
Petros Moustopoulos <pet.moustopoulos@gmail.com>
2026-08-22 17:33:58 +0300

    Initial commit with added Apache License

---
+++ src/main/java/io/github/pmoustopoulos/sftpconsole/SftpConsoleSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.pmoustopoulos.sftpconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpConsoleSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftpConsoleSpringBootStarterApplication.class, args);
+    }
+
+}
